393e09fdfba7ff45a5439618ad7c9645940c0986
Mateusz Siniarski <mateusz.siniarski@hiof.no>
2014-02-25 19:45:38 +0100

    First commit

---
+++ GPS/src/com/example/gps1/GPSService.java
@@ -0,0 +1,142 @@
+package com.example.gps1;
+
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+
+public class GPSService extends Service {
+
+	int _notificationId = 1;
+	int _callCount = 0;
+	private String tag = "Livssyklus";
+	private String gps_data = "gps_data";
+	
+	LocationManager lm;
+	LocationListener locationListener;
+	Intent runServiceIntent;
+	
+	
+	
+	
+	@Override
+	public void onCreate() {
+		Log.i(tag, "onCreate");
+		_callCount = 0;
+		lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+		locationListener = new MyLocationListener();
+		startInForeground();
+	}
+	
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId)
+	{
+		_callCount++;
+		Log.i(tag, "onStartCommand - call #" + _callCount);
+		
+		lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1, locationListener);
+		
+		
+		
+		return START_NOT_STICKY;
+	}
+	
+	private void startInForeground()
+	{
+		// Set basic notification information
+		int notificationIcon = R.drawable.ic_launcher;
+		String notificationTickerText = "Launching my service";
+		long notificationTimeStamp = System.currentTimeMillis();
+		
+		// Describe what to do if the user clicks on the notification in the status bar
+		String notificationTitleText = "My Service";
+        String notificationBodyText = "Does non-UI processing";
+        Intent notificationActivityIntent = new Intent(this, MainActivity.class);
+        notificationActivityIntent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        PendingIntent startMyActivityPendingIntent = PendingIntent.getActivity(this, 0, notificationActivityIntent, 0);
+		
+        Notification foregroundNotification = null;
+        
+		NotificationCompat.Builder notificationbuilder = new NotificationCompat.Builder(this)
+														.setSmallIcon(notificationIcon)
+														.setTicker(notificationTickerText)
+													    .setWhen(notificationTimeStamp);
+			
+			foregroundNotification = notificationbuilder.setContentTitle(notificationTitleText)
+											.setContentText(notificationBodyText)
+											.setContentIntent(startMyActivityPendingIntent).build();
+		
+        // ID to use w/ Notification Manager for _foregroundNotification
+        // Set the service to foreground status and provide notification info
+        startForeground(_notificationId, foregroundNotification);
+	}
+	
+	@Override
+	public IBinder onBind(Intent intent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	public class MyLocationListener implements LocationListener {
+
+    	@Override
+    	public void onLocationChanged(Location loc) {
+    		
+    		//TextView t1 = (TextView) findViewById(R.id.textView_Latitude);
+    		String s_latitude = "Lat: "+ Double.toString(loc.getLatitude());
+            MainActivity.latitude = (s_latitude);
+            Log.i(gps_data,s_latitude);
+            
+            //TextView t2 = (TextView) findViewById(R.id.textView_Longitude);
+            String s_longitude = "Lng: " +Double.toString(loc.getLongitude());
+            MainActivity.longitude = (s_longitude);
+            Log.i(gps_data,s_longitude);
+            
+            //TextView t3 = (TextView) findViewById(R.id.textView_Altitude);
+            String s_altitude = "Alt: "+Double.toString(loc.getAltitude());
+            MainActivity.altitude = (s_altitude);
+            Log.i(gps_data,s_longitude);
+            
+            //TextView t4 = (TextView) findViewById(R.id.textView_Speed);
+            double km = 3.6*loc.getSpeed();
+            String s_speed = "Speed: "+Double.toString(km)+" km/h";
+            MainActivity.speed = (s_speed);
+            Log.i(gps_data,s_speed);
+            
+            //MainActivity.updateValues();
+            
+            
+
+    	}
+
+    	@Override
+    	public void onProviderDisabled(String arg0) {
+    		// TODO Auto-generated method stub
+
+    	}
+
+    	@Override
+    	public void onProviderEnabled(String arg0) {
+    		// TODO Auto-generated method stub
+
+    	}
+
+    	@Override
+    	public void onStatusChanged(String arg0, int arg1, Bundle arg2) {
+    		// TODO Auto-generated method stub
+
+    	}
+
+    }
+}
+
+
---
+++ GPS/src/com/example/gps1/MainActivity.java
@@ -0,0 +1,170 @@
+package com.example.gps1;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+	private String tag = "Livssyklus";
+	LocationManager lm;
+	LocationListener locationListener;
+	Intent runServiceIntent;
+	
+	public static String latitude;
+	public static String longitude;
+	public static String altitude;
+	public static String speed;
+	
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		Log.d(tag, "Inne i onCreate()");
+		
+	}
+	
+	public void startButton(View v) {
+				//startActivity(new Intent("android.intent.action.secondActivity"));
+				Log.d(tag, "Start knappen er trykket!");
+				lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1, locationListener);
+	}
+	
+	public void stopButton(View v) {
+				//startActivity(new Intent("android.intent.action.secondActivity"));
+				Log.d(tag, "Stopp knappen er trykket!");
+				lm.removeUpdates(locationListener);
+	}
+	
+	public void startService(View v) {
+		
+		Log.d(tag, "Start Service knapp trykket");
+		runServiceIntent = new Intent("com.example.gps1.action.LOG_POS");
+		startService(runServiceIntent);
+	}
+	
+	public void stopService(View v) {
+		
+		Log.d(tag, "Stop Service knapp trykket");
+		
+		if(runServiceIntent != null)	{
+			stopService(runServiceIntent);
+			runServiceIntent = null;
+		}
+	}
+	
+	protected void getLocation()	{
+		
+		//LocationListener locationListener = new MyLocationListener();
+		
+	}
+	/*
+	public void doSomething(View view)
+	{
+		//startActivity(new Intent("com.capgemini.second_activity"));
+		Log.d(tag, "Knappen er trykket!");
+	}*/
+
+	@Override
+	protected void onStart()
+	{
+		super.onStart();
+		Log.d(tag, "Inne i onStart()");
+	}
+
+	@Override
+	protected void onResume()
+	{
+		super.onResume();
+		Log.d(tag, "Inne i onResume()");
+	}
+
+	@Override
+	protected void onPause()
+	{
+		super.onPause();
+		Log.d(tag, "Inne i onPause()");
+	}
+
+	@Override
+	protected void onStop()
+	{
+		super.onStop();
+		Log.d(tag, "Inne i onStop()");
+	}
+
+	@Override
+	protected void onDestroy()
+	{
+		super.onDestroy();
+		Log.d(tag, "Inne i onDestroy()");
+	}
+	
+/*
+	@Override
+	/public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}*/
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+    /*
+    private class MyLocationListener implements LocationListener {
+
+    	@Override
+    	public void onLocationChanged(Location arg0) {
+    		
+    		TextView t1 = (TextView) findViewById(R.id.textView_Latitude);
+            t1.setText("Lat: "+ Double.toString(arg0.getLatitude()));
+            
+            TextView t2 = (TextView) findViewById(R.id.textView_Longitude);
+            t2.setText("Lng: " +Double.toString(arg0.getLongitude()));
+            
+            TextView t3 = (TextView) findViewById(R.id.textView_Altitude);
+            t3.setText("Alt: "+Double.toString(arg0.getAltitude()));
+            
+            TextView t4 = (TextView) findViewById(R.id.textView_Speed);
+            double km = 3.6*arg0.getSpeed();
+            t4.setText("Speed: "+Double.toString(km)+" km/h");
+            
+            
+
+    	}
+
+    	@Override
+    	public void onProviderDisabled(String arg0) {
+    		// TODO Auto-generated method stub
+
+    	}
+
+    	@Override
+    	public void onProviderEnabled(String arg0) {
+    		// TODO Auto-generated method stub
+
+    	}
+
+    	@Override
+    	public void onStatusChanged(String arg0, int arg1, Bundle arg2) {
+    		// TODO Auto-generated method stub
+
+    	}
+
+    }*/
+    
+}
